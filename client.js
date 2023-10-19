45182ed7a3563d621fc8c3458cd81af60b4617b8
satyamkumarjha9966 <satyamkumarjha9966@gmail.com>
2023-10-19 21:51:54 +0530

    TCP-IP-Client Server Interaction Demo

--- client.js
+++ client.js
@@ -2,12 +2,11 @@ const net = require("net");
 const port = 7070;
 const host = "localhost";
 
-
 const client = new net.Socket();
 client.connect(port, host, function () {
   // Data That You Want To Send To TCP Server
   client.write(
-    `satyam,001,003,000,001,008,016,210623 175542 ,,Z0000,0000,0000,0000#`
+    `satyam,001,003,000,001,008,016,210623 175542 ,,Z0000,0000,0000,0000,0000,0000#`
   );
   client.destroy();
 });
@@ -15,4 +14,3 @@ client.connect(port, host, function () {
 client.on("error", function (error) {
   console.log(error.message);
 });
-
